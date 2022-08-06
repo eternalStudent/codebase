cc1a9b6d9d9eff801d53d8ecf85e5b7bf9a07299
eternalStudent <noam.weisberg@gmail.com>
2022-08-06 20:05:48 +0300

    fix time.cpp

--- basic/time.cpp
+++ basic/time.cpp
@@ -1,7 +1,8 @@
-#include "win32time.cpp"
-
-#define OsTimeInit		Win32TimeInit
-#define OsTimeStart 	Win32TimeStart
-#define OsTimePause 	Win32TimePause
-#define OsTimeStop		Win32TimeStop
-#define OsTimeRestart 	Win32TimeRestart
+#if defined(_WIN32)
+#  include "win32time.cpp"
+#  define OsTimeInit		Win32TimeInit
+#  define OsTimeStart 		Win32TimeStart
+#  define OsTimePause 		Win32TimePause
+#  define OsTimeStop		Win32TimeStop
+#  define OsTimeRestart 	Win32TimeRestart
+#endif
