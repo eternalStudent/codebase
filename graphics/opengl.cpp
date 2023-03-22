7cb2bfbc1b00680ceef0dc4fea4bdf0132e98395
eternalStudent <noam.weisberg@gmail.com>
2023-03-22 14:36:58 +0200

    remove commented put code, and other minor changes

--- graphics/opengl.cpp
+++ graphics/opengl.cpp
@@ -50,6 +50,149 @@ GLuint CreateProgram(GLchar* vertexSource, GLchar* fragmentSource) {
 	return program;
 }
 
+GLuint CreateQuadProgram() {
+	GLchar* vertexSource = (GLchar*)R"STRING(
+#version 330 core
+
+layout (location = 0) in vec2 in_position;
+layout (location = 1) in vec2 in_center;
+layout (location = 2) in vec2 in_size;
+layout (location = 3) in float in_radius;
+layout (location = 4) in float in_borderWidth;
+layout (location = 5) in vec4 in_color;
+layout (location = 6) in vec4 in_borderColor;
+
+out vec2 position;
+out vec2 center;
+out vec2 size;
+out float radius;
+out float borderWidth;
+out vec4 color;
+out vec4 borderColor;
+
+uniform mat4 mvp;
+
+void main() {
+	gl_Position = mvp * vec4(in_position, 0.0, 1.0);
+
+	position = in_position;
+	center = in_center;
+	size = in_size;
+	radius = in_radius;
+	borderWidth = in_borderWidth;
+	color = in_color;
+	borderColor = in_borderColor;
+}
+	)STRING";
+
+	GLchar* fragmentSource = (GLchar*)R"STRING(
+#version 330 core
+
+in vec2 position;
+in vec2 center;
+in vec2 size;
+in float radius;
+in float borderWidth;
+in vec4 color;
+in vec4 borderColor;
+
+out vec4 out_color;
+
+float sd(vec2 pos, vec2 halfSize, float radius) {
+	pos = abs(pos) - halfSize + radius;
+	return length(max(pos, 0)) + min(max(pos.x, pos.y), 0) - radius;
+}
+
+void main() {
+	float sd_from_outer = sd(position - center, size/2.f, radius) + 0.5;
+	float sd_from_inner = sd_from_outer + borderWidth;
+	
+    float a1 = 1.0f - smoothstep(0.0f, 1.0f, sd_from_outer);
+    float a2 = 1.0f - smoothstep(0.0f, 1.0f, sd_from_inner);
+	
+	out_color = mix(borderColor, color, a2);
+	out_color.a = out_color.a*a1;
+}   
+	)STRING";
+
+	return CreateProgram(vertexSource, fragmentSource);
+}
+
+GLuint CreateTextProgram() {
+	GLchar* vertexSource = (GLchar*)R"STRING(
+#version 330 core
+
+layout (location = 0) in vec2 in_position;
+layout (location = 1) in vec2 in_uv;
+layout (location = 2) in vec4 in_color;
+
+out vec2 uv;
+out vec4 color;
+
+uniform mat4 mvp;
+
+void main() {
+	uv = in_uv;
+	color = in_color;
+
+	gl_Position = mvp * vec4(in_position, 0.0, 1.0);
+}
+	)STRING";
+
+	GLchar* fragmentSource = (GLchar*)R"STRING(
+#version 330 core
+
+in vec2 uv;
+in vec4 color;
+
+out vec4 out_color;
+
+uniform sampler2D atlas;
+
+void main() {
+	float a = texture(atlas, uv).r;
+    out_color = vec4(color.rgb, color.a*a);
+}   
+	)STRING";
+
+	return CreateProgram(vertexSource, fragmentSource);
+}
+
+GLuint CreateImageProgram() {
+	GLchar* vertexSource = (GLchar*)R"STRING(
+#version 330 core
+
+layout (location = 0) in vec2 in_position;
+layout (location = 1) in vec2 in_uv;
+
+out vec2 uv;
+
+uniform mat4 mvp;
+
+void main() {
+	uv = in_uv;
+
+	gl_Position = mvp * vec4(in_position, 0.0, 1.0);
+}
+	)STRING";
+
+	GLchar* fragmentSource = (GLchar*)R"STRING(
+#version 330 core
+
+in vec2 uv;
+
+out vec4 out_color;
+
+uniform sampler2D image;
+
+void main() {
+	out_color = texture(image, uv);
+}   
+	)STRING";
+
+	return CreateProgram(vertexSource, fragmentSource);
+}
+
 GLuint OpenGLGenerateTexture(Image image, GLint filter) {
 	GLuint texture;
 	glGenTextures(1, &texture);
@@ -73,486 +216,3 @@ GLuint OpenGLGenerateTexture(Image image, GLint filter) {
 
 	return texture;
 }
-
-void OpenGLUpdateTextureData(GLuint texture, int32 xoffset, int32 yoffset, int32 width, int32 height, void* data) {
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexSubImage2D(GL_TEXTURE_2D, 0, xoffset, yoffset, width, height, GL_RGBA, GL_UNSIGNED_BYTE, data);
-}
-
-/* NOTE:
- *-------
- * This code no longer represents my current understanding of opengl.
- * Many of the implementations here are not very good, and will be replaced once
- * I fill comfortable enough with my graphics programming knowledge.
- * see the code in uigraphics.cpp
- */
-
-/*
-struct {
-	GLuint drawImage;
-	GLuint drawShape;
-	GLuint drawText;
-	uint32 rgba;
-	Arena* arena;
-} opengl;
-
-void OpenGLInit(Arena* arena) {
-	OSOpenGLInit();
-
-	opengl.arena = arena;
-
-	GLchar* vertexSource = (GLchar*)R"STRING(
-#version 330 core
-layout (location = 0) in vec4 vertex; // <vec2 position, vec2 texCoords>
-
-out vec2 TexCoords;
-
-void main()
-{
-    TexCoords = vertex.zw;
-    gl_Position = vec4(vertex.xy, 0.0, 1.0);
-}
-	)STRING";
-
-	GLchar* fragmentSource = (GLchar*)R"STRING(
-#version 330 core
-in vec2 TexCoords;
-out vec4 color;
-
-uniform sampler2D image;
-
-void main()
-{    
-    color = texture(image, TexCoords);
-} 
-	)STRING";
-	opengl.drawImage = CreateProgram(vertexSource, fragmentSource);
-
-	fragmentSource = (GLchar*)R"STRING(
-#version 330 core
-in vec2 TexCoords;
-out vec4 color;
-  
-uniform vec4 background;
-uniform vec4 foreground;
-uniform sampler2D image;
-
-void main()
-{
-	float channel = texture(image, TexCoords).r;
-    color = channel*foreground + (1.0-channel)*background;
-}   
-	)STRING";
-	opengl.drawText = CreateProgram(vertexSource, fragmentSource);
-
-	vertexSource = (GLchar*)R"STRING(
-#version 330 core
-layout (location = 0) in vec2 vertex;
-
-void main()
-{
-    gl_Position = vec4(vertex, 0.0, 1.0);
-}
-	)STRING";
-
-	fragmentSource = (GLchar*)R"STRING(
-#version 330 core
-out vec4 FragColor;
-  
-uniform vec4 color;
-
-void main()
-{
-    FragColor = color;
-}   
-	)STRING";
-	opengl.drawShape = CreateProgram(vertexSource, fragmentSource);
-
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_LINE_SMOOTH);
-	glEnable(GL_SCISSOR_TEST);
-
-#if DEBUG
-	glEnable(GL_DEBUG_OUTPUT);
-	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-#endif
-}
-
-void OpenGLSetColor(uint32 rgba) {
-	opengl.rgba = rgba;
-}
-
-void OpenGLClearScreen() {
-	Dimensions2i dim = OSGetWindowDimensions();
-	glScissor(0, 0, dim.width, dim.height);
-	Color color = RgbaToColor(opengl.rgba);
-	glClearColor(color.r, color.g, color.b, color.a);
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glViewport(0, 0, dim.width, dim.height);
-}
-
-Point2 AdjustCoordinates(Point2 p) {
-	// TODO: maybe add these to Window?
-	Dimensions2i dim = OSGetWindowDimensions();
-	float32 iwidth = 1.0f/dim.width;
-	float32 iheight = 1.0f/dim.height;
-
-	return {(2.0f*p.x*iwidth) - 1, (2.0f*p.y*iheight) - 1};
-}
-
-void OpenGLDrawImage(GLuint texture, Box2 crop, Box2 pos) {
-	Point2 p0 = AdjustCoordinates(pos.p0);
-	Point2 p1 = AdjustCoordinates(pos.p1);
-
-	float32 bottomTex = crop.y0;
-	float32 topTex = crop.y1;
-	float32 rightTex = crop.x1;
-	float32 leftTex = crop.x0;
-
-	float32 vertices[] = {
-		// pos            // tex
-		p1.x, p0.y,       rightTex, bottomTex,   // bottom-right
-		p0.x, p1.y,       leftTex, topTex,       // top-left
-		p0.x, p0.y,       leftTex, bottomTex,    // bottom-left
-
-		p0.x, p1.y,       leftTex, topTex,       // top-left
-		p1.x, p1.y,       rightTex, topTex,      // top-right
-		p1.x, p0.y,       rightTex, bottomTex    // bottom-right
-	};
-
-	GLsizeiptr size = sizeof(vertices);
-	GLuint buffersHandle;
-	glGenBuffers(1, &buffersHandle);
-	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle);
-	glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
-
-	GLuint verticesHandle;
-	glGenVertexArrays(1, &verticesHandle);
-	glBindVertexArray(verticesHandle);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4*sizeof(float32), NULL);
-
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	glUseProgram(opengl.drawImage);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glUniform1i(glGetUniformLocation(opengl.drawImage, "image"), 0);
-
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-
-	glDeleteVertexArrays(1, &verticesHandle);
-	glDeleteBuffers(1, &buffersHandle);
-}
-
-void OpenGLDrawText(GLuint texture, Box2 crop, Box2 pos, Color background, Color foreground) {
-	Point2 p0 = AdjustCoordinates(pos.p0);
-	Point2 p1 = AdjustCoordinates(pos.p1);
-
-	float32 bottomTex = crop.y0;
-	float32 topTex = crop.y1;
-	float32 rightTex = crop.x1;
-	float32 leftTex = crop.x0;
-
-	float32 vertices[] = {
-		// pos            // tex
-		p1.x, p0.y,       rightTex, bottomTex,   // bottom-right
-		p0.x, p1.y,       leftTex, topTex,       // top-left
-		p0.x, p0.y,       leftTex, bottomTex,    // bottom-left
-
-		p0.x, p1.y,       leftTex, topTex,       // top-left
-		p1.x, p1.y,       rightTex, topTex,      // top-right
-		p1.x, p0.y,       rightTex, bottomTex    // bottom-right
-	};
-
-	GLsizeiptr size = sizeof(vertices);
-	GLuint buffersHandle;
-	glGenBuffers(1, &buffersHandle);
-	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle);
-	glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
-
-	GLuint verticesHandle;
-	glGenVertexArrays(1, &verticesHandle);
-	glBindVertexArray(verticesHandle);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4*sizeof(float32), NULL);
-
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	glUseProgram(opengl.drawText);
-	
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glUniform1i(glGetUniformLocation(opengl.drawText, "image"), 0);
-
-	GLint location = glGetUniformLocation(opengl.drawText, "background");
-	glUniform4f(location, background.r, background.g, background.b, background.a);
-
-	location = glGetUniformLocation(opengl.drawText, "foreground");
-	glUniform4f(location, foreground.r, foreground.g, foreground.b, foreground.a);
-
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-
-	glDeleteVertexArrays(1, &verticesHandle);
-	glDeleteBuffers(1, &buffersHandle);
-}
-
-void OpenGLDrawText(GLuint texture, Box2 crop, Box2 pos, uint32 background, uint32 foreground) {
-	OpenGLDrawText(texture, crop, pos, RgbaToColor(background), RgbaToColor(foreground));
-}	
-
-void OpenGLDrawBox2(Box2 pos, uint32 rgba) {
-	Point2 p0 = AdjustCoordinates(pos.p0);
-	Point2 p1 = AdjustCoordinates(pos.p1);
-
-	float32 vertices[] = {
-		p0.x, p0.y,
-		p0.x, p1.y,
-		p1.x, p1.y,
-		p1.x, p0.y
-	};
-
-	GLsizeiptr size = sizeof(vertices);
-	GLuint buffersHandle;
-	glGenBuffers(1, &buffersHandle);
-	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle);
-	glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
-
-	GLuint verticesHandle;
-	glGenVertexArrays(1, &verticesHandle);
-	glBindVertexArray(verticesHandle);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float32), NULL);
-
-	GLint location = glGetUniformLocation(opengl.drawShape, "color");
-	Color color = RgbaToColor(rgba);
-	glUseProgram(opengl.drawShape);
-	glUniform4f(location, color.r, color.g, color.b, color.a);
-
-	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-
-	glDeleteVertexArrays(1, &verticesHandle);
-	glDeleteBuffers(1, &buffersHandle);
-}
-
-void OpenGLDrawTriangle(Triangle triangle, uint32 rgba) {
-	Point2 p0 = AdjustCoordinates(triangle.p0);
-	Point2 p1 = AdjustCoordinates(triangle.p1);
-	Point2 p2 = AdjustCoordinates(triangle.p2);
-
-	float32 vertices[] = {
-		p0.x, p0.y,
-		p1.x, p1.y,
-		p2.x, p2.y
-	};
-
-	GLsizeiptr size = sizeof(vertices);
-	GLuint buffersHandle;
-	glGenBuffers(1, &buffersHandle);
-	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle);
-	glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
-
-	GLuint verticesHandle;
-	glGenVertexArrays(1, &verticesHandle);
-	glBindVertexArray(verticesHandle);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float32), NULL);
-
-	GLint location = glGetUniformLocation(opengl.drawShape, "color");
-	Color color = RgbaToColor(rgba);
-	glUseProgram(opengl.drawShape);
-	glUniform4f(location, color.r, color.g, color.b, color.a);
-
-	glDrawArrays(GL_TRIANGLE_FAN, 0, 3);
-
-	glDeleteVertexArrays(1, &verticesHandle);
-	glDeleteBuffers(1, &buffersHandle);
-}
-
-void OpenGLDrawLine(Line2 line, float32 lineWidth, uint32 rgba) {
-	ArenaAlign(opengl.arena, 4);
-	float32* vertices = (float32*)ArenaAlloc(opengl.arena, 2*line.count*sizeof(float32));
-
-	for (int32 i = 0; i<line.count; i++) {
-		Point2 p = AdjustCoordinates(line.points[i]);
-		vertices[i*2  ] = p.x;
-		vertices[i*2+1] = p.y;
-	}
-
-	GLuint buffersHandle;
-	glGenBuffers(1, &buffersHandle);
-	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle);
-	glBufferData(GL_ARRAY_BUFFER, 2*line.count*sizeof(float32), vertices, GL_STATIC_DRAW);
-
-	GLuint verticesHandle;
-	glGenVertexArrays(1, &verticesHandle);
-	glBindVertexArray(verticesHandle);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float32), NULL);
-
-	GLint location = glGetUniformLocation(opengl.drawShape, "color");
-	Color color = RgbaToColor(rgba);
-	glUseProgram(opengl.drawShape);
-	glUniform4f(location, color.r, color.g, color.b, color.a);
-	glLineWidth(lineWidth ? lineWidth : 1);
-
-	glDrawArrays(GL_LINE_STRIP, 0, line.count);
-
-	glDeleteVertexArrays(1, &verticesHandle);
-	glDeleteBuffers(1, &buffersHandle);
-}
-
-void OpenGLDrawCircle(Sphere2 circle, float32 lineWidth, uint32 rgba, int32 startAngle, int32 endAngle) {
-	int32 count = (endAngle-startAngle+1);
-	float32 vertices[362*2];
-	for (int32 i=0; i<=count; i++) {
-		int32 deg = i+startAngle;
-		float32 rad = deg*DEG_TO_RAD();
-		Point2 p = AdjustCoordinates(PointOnSphere(circle, rad));
-
-		vertices[i*2  ] = p.x;
-		vertices[i*2+1] = p.y;
-	}
-
-	GLuint buffersHandle;
-	glGenBuffers(1, &buffersHandle);
-	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle);
-	glBufferData(GL_ARRAY_BUFFER, 2*count*sizeof(float32), vertices, GL_STATIC_DRAW);
-
-	GLuint verticesHandle;
-	glGenVertexArrays(1, &verticesHandle);
-	glBindVertexArray(verticesHandle);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float32), NULL);
-
-	GLint location = glGetUniformLocation(opengl.drawShape, "color");
-	Color color = RgbaToColor(rgba);
-	glUseProgram(opengl.drawShape);
-	glUniform4f(location, color.r, color.g, color.b, color.a);
-	glLineWidth(lineWidth ? lineWidth : 1);
-
-	glDrawArrays(GL_LINE_STRIP, 0, count);
-
-	glDeleteVertexArrays(1, &verticesHandle);
-	glDeleteBuffers(1, &buffersHandle);
-}
-
-void OpenGLDrawDisc(Sphere2 disc, uint32 rgba, int32 startAngle, int32 endAngle) {
-	int32 count = (endAngle-startAngle+1);
-	float32 vertices[363*2];
-	Point2 center = AdjustCoordinates(disc.center);
-	vertices[0] = center.x;
-	vertices[1] = center.y;
-	for (int32 i=0; i<=count; i++) {
-		int32 j = (i+1)*2;
-		int32 deg = i+startAngle;
-		float32 rad = deg*DEG_TO_RAD();
-		Point2 p = AdjustCoordinates(PointOnSphere(disc, rad));
-		vertices[j  ] = p.x;
-		vertices[j+1] = p.y;
-	}
-
-	GLuint buffersHandle;
-	glGenBuffers(1, &buffersHandle);
-	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle);
-	glBufferData(GL_ARRAY_BUFFER, 2*(count+1)*sizeof(float32), vertices, GL_STATIC_DRAW);
-
-	GLuint verticesHandle;
-	glGenVertexArrays(1, &verticesHandle);
-	glBindVertexArray(verticesHandle);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float32), NULL);
-
-	GLint location = glGetUniformLocation(opengl.drawShape, "color");
-	Color color = RgbaToColor(rgba);
-	glUseProgram(opengl.drawShape);
-	glUniform4f(location, color.r, color.g, color.b, color.a);
-
-	glDrawArrays(GL_TRIANGLE_FAN, 0, count+1);
-
-	glDeleteVertexArrays(1, &verticesHandle);
-	glDeleteBuffers(1, &buffersHandle);
-}
-
-void OpenGLDrawCurve3(Point2 p0, Point2 p1, Point2 p2, float32 lineWidth, uint32 rgba) {
-	float32 vertices[257*2];
-
-	float32 fraction = LoadExp(-8);
-	for (int32 i = 0; i<=256; i++) {
-		float32 t = i*fraction;
-		Point2 p = AdjustCoordinates(Qerp2(t, p0, p1, p2));
-		vertices[i*2  ] = p.x;
-		vertices[i*2+1] = p.y;
-	}
-
-	GLuint buffersHandle;
-	glGenBuffers(1, &buffersHandle);
-	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle);
-	glBufferData(GL_ARRAY_BUFFER, 2*257*sizeof(float32), vertices, GL_STATIC_DRAW);
-
-	GLuint verticesHandle;
-	glGenVertexArrays(1, &verticesHandle);
-	glBindVertexArray(verticesHandle);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float32), NULL);
-
-	GLint location = glGetUniformLocation(opengl.drawShape, "color");
-	Color color = RgbaToColor(rgba);
-	glUseProgram(opengl.drawShape);
-	glUniform4f(location, color.r, color.g, color.b, color.a);
-	glLineWidth(lineWidth ? lineWidth : 1);
-
-	glDrawArrays(GL_LINE_STRIP, 0, 257);
-
-	glDeleteVertexArrays(1, &verticesHandle);
-	glDeleteBuffers(1, &buffersHandle);
-}
-
-void OpenGLDrawCurve4(Point2 p0, Point2 p1, Point2 p2, Point2 p3, float32 lineWidth, uint32 rgba) {
-	float32 vertices[257*2];
-
-	float32 fraction = LoadExp(-8);
-	for (int32 i = 0; i<=256; i++) {
-		float32 t = i*fraction;
-		Point2 p = AdjustCoordinates(Cerp2(t, p0, p1, p2, p3));
-		vertices[i*2  ] = p.x;
-		vertices[i*2+1] = p.y;
-	}
-
-	GLuint buffersHandle;
-	glGenBuffers(1, &buffersHandle);
-	glBindBuffer(GL_ARRAY_BUFFER, buffersHandle);
-	glBufferData(GL_ARRAY_BUFFER, 2*257*sizeof(float32), vertices, GL_STATIC_DRAW);
-
-	GLuint verticesHandle;
-	glGenVertexArrays(1, &verticesHandle);
-	glBindVertexArray(verticesHandle);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float32), NULL);
-
-	GLint location = glGetUniformLocation(opengl.drawShape, "color");
-	Color color = RgbaToColor(rgba);
-	glUseProgram(opengl.drawShape);
-	glUniform4f(location, color.r, color.g, color.b, color.a);
-	glLineWidth(lineWidth ? lineWidth : 1);
-
-	glDrawArrays(GL_LINE_STRIP, 0, 257);
-
-	glDeleteVertexArrays(1, &verticesHandle);
-	glDeleteBuffers(1, &buffersHandle);
-}
-
-void OpenGlClearCrop() {
-	Dimensions2i dim = OSGetWindowDimensions();
-	glScissor(0, 0, dim.width, dim.height);
-}
-*/
